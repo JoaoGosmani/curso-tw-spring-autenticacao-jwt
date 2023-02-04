b55e4dcd6c625f129cb642d9e5fae045f7ec6075
Joao Gosmani <joaogosmani@gmail.com>
2023-02-04 16:48:36 -0300

    Projeto criado

---
+++ src/test/java/br/com/joaogosmani/javajobs/JavajobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.javajobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
